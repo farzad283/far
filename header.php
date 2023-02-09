eb09d26b500461a166ec1f4a36225256409fa3ca
Farzad <farzad@Farzads-MacBook-Air.local>
2023-02-09 15:50:27 -0500

    tentth try

--- header.php
+++ header.php
@@ -10,6 +10,13 @@
 <header>
     <img src="http://localhost:8888/31w/wp-content/uploads/2023/02/index.png" alt="">
     <h1>Habibzadeh Weblog</h1>
+
+    <h1><?php  bloginfo('name'); ?></h1>
+    <h2><?php  bloginfo('description'); ?></h2>
+    <h2><?php  bloginfo('url'); ?></h2>
+    <h1><a href="<?php  bloginfo('url'); ?>"><?php  bloginfo('name'); ?></a></h1> 
+
+
 </header>
 <body>
 
