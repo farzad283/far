24ee1687611493f2c4bec7ed4a026ff7923c94de
Farzad <farzad@Farzads-MacBook-Air.local>
2023-02-14 14:37:40 -0500

    eleventh try

--- front-page.php
+++ front-page.php
@@ -11,7 +11,7 @@
 <a href="#">Wordpress</a>
 </nav>
 <main>
-    <h3>index.php</h3>
+    <h3>front-page.php</h3>
 <?php 
 if (have_posts()):
     while( have_posts()) : the_post();
@@ -26,26 +26,5 @@ if (have_posts()):
     endwhile;
 endif;
 ?>
-
-<section>
-    <ul>
-        <li>Java Script</li>
-        <li>CSS</li>
-        <li>HTML</li>
-        <li>PHP</li>
-    </ul>
-    <table border="1">
-        <tr>
-        <th>Course 1</th>
-        <th>Course 2</th>
-        <th>Course 3</th>
-        </tr>
-        <tr>
-        <td>25 h</td>
-        <td>38h</td>
-        <td>60 h</td>
-        </tr>
-    </table>
-</section>
 </main>
 <?php get_footer(); ?>
