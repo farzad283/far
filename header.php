701c1ad4fe188f4a9a39d23c5be9e335356f83e0
Farzad <farzad@Farzads-MacBook-Air.local>
2023-02-15 10:57:02 -0500

    thirteenth try

--- header.php
+++ header.php
@@ -8,15 +8,18 @@
     <?php wp_head();?>
 </head>
 <header>
+<div>
     <img src="http://localhost:8888/31w/wp-content/uploads/2023/02/index.png" alt="">
-
-    <h1>M-Habibzadeh</h1>
-    <h1><?php  bloginfo('name'); ?></h1>
-    <h2><?php  //bloginfo('description'); ?></h2>
+        <h1>M-Habibzadeh</h1>
+    </div>
+    <?php wp_nav_menu(array(
+        'menu' => 'entete',
+        'container' => 'nav'
+    )); ?>
+    <h1><?php  //bloginfo('name'); ?></h1>
     <h2><?php  //bloginfo('url'); ?></h2>
-    <h1><a href="<?php  bloginfo('url'); ?>"><?php  //bloginfo('name'); ?></a></h1> 
-
-
+    <h1><a href="<?php  bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1> 
+    <h2><?php  bloginfo('description'); ?></h2>
 </header>
 <body>
 
---
+++ readme.md
@@ -0,0 +1,7 @@
+Mohammadreza Habibzadeh
+2296191
+
+https://aecweb9.sg-host.com/
+
+github:
+https://github.com/farzad283/Exer1
