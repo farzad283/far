ac3c539af80fd64256d8956d85959bd5e15e4e3b
farzad283 <124091597+farzad283@users.noreply.github.com>
2023-02-22 12:08:28 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head();?>
+</head>
+<header>
+<div>
+    <img src="http://localhost:8888/31w/wp-content/uploads/2023/02/index.png" alt="">
+        <h1>M-Habibzadeh</h1>
+    </div>
+    <?php wp_nav_menu(array(
+        'menu' => 'entete',
+        'container' => 'nav'
+    )); ?>
+    <h1><?php  //bloginfo('name'); ?></h1>
+    <h2><?php  //bloginfo('url'); ?></h2>
+    <h1><a href="<?php  bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1> 
+    <h2><?php  bloginfo('description'); ?></h2>
+</header>
+<body>
+
+
+
+
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Modele index.php représente le modéle par défault du thème
+*/
+?>
+
+<?php get_header()?>
+<nav>
+<a href="#">Java</a>
+<a href="#">PHP</a>
+<a href="#">Wordpress</a>
+</nav>
+<main>
+    <h3>index.php</h3>
+<?php 
+if (have_posts()):
+    while( have_posts()) : the_post();
+        //the_title('<h1>','</h1>');
+        //the_permalink();?>
+
+        <h1><a href="<?php the_permalink(); ?>"><?= get_the_title() ?></a></h1>
+<?php 
+        //the_content();
+        //the_excerpt();
+        echo wp_trim_words(get_the_excerpt(), 4);
+    endwhile;
+endif;
+?>
+
+</main>
+<?php get_footer(); ?>
