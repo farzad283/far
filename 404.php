cf3645fdc6ba9f2dcd57be8f0de7baa4d9832c4c
farzad283 <124091597+farzad283@users.noreply.github.com>
2023-02-22 11:45:21 -0500

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Modele index.php représente le modéle par défault du thème
+*/
+?>
+
+<?php get_header()?>
+<main>
+    <code>404.php</code>
+    <h1>Erreur 404 - La page n'existe pas</h1>
+
+</main>
+<?php get_footer(); ?>
